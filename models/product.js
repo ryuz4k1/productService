f42ddeb2c502b22306dc3654649c8d2bb3f674df
James Ford <33656801+ryuz4k1@users.noreply.github.com>
2019-06-22 17:31:22 +0300

    Delete product.js

--- models/product.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const product = sequelize.define('product', {
-    
-  }, {});
-  product.associate = function(models) {
-  };
-  return product;
-};
